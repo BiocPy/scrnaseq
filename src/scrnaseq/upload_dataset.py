7f4c607e003087c9e7a0bd74cd2f60b53cebaeee
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-05-23 08:05:45 -0700

    docstring for upload

--- src/scrnaseq/upload_dataset.py
+++ src/scrnaseq/upload_dataset.py
@@ -1,14 +1,4 @@
-import json
-import os
-import shutil
-from functools import singledispatch
-from typing import Any
-
-import dolomite_base as dl
 from gypsum_client import cache_directory, rest_url, upload_directory
-from singlecellexperiment import SingleCellExperiment
-
-from .utils import format_object_metadata
 
 __author__ = "Jayaram Kancherla"
 __copyright__ = "Jayaram Kancherla"
@@ -28,6 +18,61 @@ def upload_dataset(
     concurrent: int = 1,
     abort_failed: bool = True,
 ):
+    """Upload the dataset to the gypsum bucket.
+
+    This is a wrapper around
+    :py:func:`~gypsum_client.upload_file_actions.upload_directory`
+    specific to the `scRNAseq` package.
+
+    See Also:
+        :py:func:`~gypsum_client.upload_file_actions.upload_directory`,
+        to upload a directory to the gypsum backend.
+
+    Args:
+        Name:
+            Dataset name.
+
+        version:
+            Version name.
+
+        directory:
+            Path to a directory containing the ``files`` to be uploaded.
+            This directory is assumed to correspond to a version of an asset.
+
+        cache_dir:
+            Path to the cache for saving files, e.g., in
+            :py:func:`~gypsum_client.save_operations.save_version`.
+
+            Used to convert symbolic links to upload links,see
+            :py:func:`~gypsum_client.prepare_directory_for_upload.prepare_directory_upload`.
+
+        deduplicate:
+            Whether the backend should attempt deduplication of ``files``
+            in the immediately previous version.
+            Defaults to True.
+
+        probation:
+            Whether to perform a probational upload.
+            Defaults to False.
+
+        url:
+            URL of the gypsum REST API.
+
+        token:
+            GitHub access token to authenticate to the gypsum REST API.
+
+        concurrent:
+            Number of concurrent downloads.
+            Defaults to 1.
+
+        abort_failed:
+            Whether to abort the upload on any failure.
+
+            Setting this to `False` can be helpful for diagnosing upload problems.
+
+    Returns:
+        `True` if successfull, otherwise `False`.
+    """
     return upload_directory(
         directory,
         package,
@@ -39,5 +84,5 @@ def upload_dataset(
         url=url,
         token=token,
         concurrent=concurrent,
-        abort_failed=abort_failed
+        abort_failed=abort_failed,
     )
